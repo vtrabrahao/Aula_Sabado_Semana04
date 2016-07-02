24d8f3b33819b2a9f2c651561b07774a500e4720
Mateus Arruda <arrudamateus3@hotmail.com>
2016-07-02 20:31:24 -0300

    first commit

---
+++ Exercicio01_Semana04/Exercicio01_Semana04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01_Semana04
+{
+    /// <summary>
+    /// Faça um programa que leia 8 variaveis e as passe como parametro para um construtor, esse construtor
+    /// deverá colocar as variaveis no vetor. E depois calcular e retornar a Média das Notas. (Construtores não possuem tipo de retorno).
+    /// </summary>
+
+    class Semana04_Notas
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 10;
+            int n2 = 10;
+            int n3 = 10;
+            int n4 = 10;
+            int n5 = 0;
+            int n6 = 0;
+            int n7 = 10;
+            int n8 = 10;
+
+
+            Vetor_Pasado oito = new Vetor_Pasado(n1,n2,n3,n4,n5,n6,n7,n8);
+            var M = oito.Retorna_Media();
+            Console.WriteLine(M);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Exercicio01_Semana04/Exercicio01_Semana04/Vetor_Pasado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio01_Semana04
+{
+    class Vetor_Pasado
+    {
+        private int[] notas;
+        public Vetor_Pasado(int np1,int np2,int np3,int np4,int np5,int np6, int np7, int np8)
+        {
+            notas = new int[] { np1, np2, np3, np4, np5, np6, np7, np8 };
+        }
+
+        public int Retorna_Media()
+        {
+            var soma = notas.Sum();
+            var M = soma / 8;
+            return M;
+        }
+    }
+}
